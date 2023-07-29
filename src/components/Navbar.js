f8ab5b49dd59f01074ff49a6aa70f21167e90ecd
obed300 <obamfo@st.ug.edu.gh>
2023-07-29 04:35:32 +0000

    Add file with content

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from '../img/contact logo.jpg';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className={styles.navbar}>
+        <img src={logo} alt="My logo" className={styles.logo} />
+        <ul className={styles.navLinks} >
+          <li>
+            <NavLink exact to="/" className={styles.active}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects" className={styles.active}>
+              User Dashboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects" className={styles.active}>
+              log in
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects" className={styles.active}>
+              Sign up
+            </NavLink>
+          </li>                    
+        </ul>
+      </nav>    
+    </div>
+  )
+}
+
+export default Navbar
