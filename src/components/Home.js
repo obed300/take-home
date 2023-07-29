455552e0236d6e2b072ed538c270c9d8794ae880
obed300 <obamfo@st.ug.edu.gh>
2023-07-29 05:07:28 +0000

    Add content to file

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/Home.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.home}>
+      <h1>Welcome to my contact App</h1>
+    </div>
+  )
+}
+
+export default Home
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../img/contact logo.jpg';
 import styles from '../styles/Navbar.module.css';
 
